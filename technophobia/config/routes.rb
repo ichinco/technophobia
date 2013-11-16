e07a4a1abcd172f4708e7d69f664bad5218c2356
Denise Ichinco <ichinco@gmail.com>
2013-11-15 21:53:47 -0500

    generated controllers and review creation page

--- technophobia/config/routes.rb
+++ technophobia/config/routes.rb
@@ -14,6 +14,11 @@ Technophobia::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :technology
+  resources :review
+  resources :language
+  resources :metareview
+
   # Example resource route with options:
   #   resources :products do
   #     member do
