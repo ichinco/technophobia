b25f6acad3b7719f4054b36de35699c3a855b5e8
Denise Ichinco <ichinco@gmail.com>
2013-11-15 21:13:26 -0500

    generated project

---
+++ technophobia/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Technophobia::Application.config.secret_key_base = 'b3cf05e00f6afb9d9deb067e99cb33b1ac450b3ab6f212d9d68cb11dccfa6568ae77858c08239d8c152e74df5a153cc67ce1b4d84e071caf69036a3daa751efa'
---
+++ technophobia/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Technophobia::Application.config.session_store :cookie_store, key: '_technophobia_session'
