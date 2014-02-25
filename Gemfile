4b3888ebe57dfbb18900462b26a3c0d2eaea90cb
Denise Ichinco <ichinco@gmail.com>
2014-02-24 22:17:05 -0500

    added admin

--- Gemfile
+++ Gemfile
@@ -54,3 +54,5 @@ gem 'font-awesome-rails'
 # gem 'debugger', group: [:development, :test]
 
 gem 'devise'
+
+gem 'activeadmin', github: 'gregbell/active_admin'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
+
 end
