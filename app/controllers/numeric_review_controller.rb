acbd0292bf6c08db628696763763d2af0b6f7b83
Denise Ichinco <ichinco@gmail.com>
2014-03-01 23:05:42 -0500

    now can only write once to db for any review type

--- app/controllers/numeric_review_controller.rb
+++ app/controllers/numeric_review_controller.rb
@@ -5,10 +5,16 @@ class NumericReviewController < ApplicationController
     @technology = Technology.find(params[:technology_id])
     @review_type = ReviewType.find(params[:review_type_id])
 
-    @text_review = NumericReview.new(review_params)
-    @text_review.technology = @technology
-    @text_review.review_type = @review_type
-    @text_review.user = current_user
+    @text_review = NumericReview.where(:user_id=>current_user.id).where(:review_type_id=>@review_type.id).where(:technology_id=>@technology.id)
+    if (@text_review.length == 0)
+      @text_review = NumericReview.new(review_params)
+      @text_review.technology = @technology
+      @text_review.review_type = @review_type
+      @text_review.user = current_user
+    else
+      @text_review = @text_review[0]
+      @text_review.review = review_params[:review]
+    end
 
     if @text_review.save
       redirect_to tech_path(@technology)
