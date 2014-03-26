f86dd5c1d630aaa0aa7c9ba0745caf2f47eb9b74
Denise Ichinco <ichinco@gmail.com>
2014-03-25 21:06:27 -0400

    add debugging for asset precompilatoin

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ group :production do
   gem 'rails_12factor'
 end
 
+gem 'sprockets_better_errors'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,4 +28,5 @@ Technophobia::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.raise_production_errors = true
 end
