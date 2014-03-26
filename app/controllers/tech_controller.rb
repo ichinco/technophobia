772b59f8997cb28c5fc940461b0e222b130285a7
Denise Ichinco <ichinco@gmail.com>
2014-03-25 20:22:18 -0400

    sped up computation of similar frameworks

--- app/controllers/tech_controller.rb
+++ app/controllers/tech_controller.rb
@@ -6,21 +6,14 @@ class TechController < ApplicationController
 
   def show
     @technology = Technology.find(params[:id])
-    @language = TechnologyType.where(:name =>"language").first
-    @language_values = @technology.technology_technology_values.collect{|value|
-      if value.technology_technology_property.value_type.id==@language.id
-        value.value
+    @languages = @technology.technology_technology_values.collect do |value|
+      if value.technology_technology_property.display_name == "Language"
+        value.value_id
       end
-    }.compact
-    @alternatives = Technology.where(:technology_type_id => @technology.technology_type_id)
-    @alternatives = @alternatives.select { |alternative| alternative.id != @technology.id}
-    @alternatives = @alternatives.select { |alternative|
-      has_language = false
-      alternative.technology_technology_values.each{ |value|
-        has_language = has_language || (value.technology_technology_property.value_type.id==@language.id && @language_values.include?(value.value))
-      }
-      has_language
-    }
+    end.compact
+    @alternatives = Technology.joins(:technology_technology_values)
+                    .where(technology_technology_values: {value_id: @languages})
+                    .where(technologies: {technology_type_id: @technology.technology_type_id})
     @rating_summary = construct_tech_overview(@technology)
     @property_values = TechnologyPropertyValue.where(:technology_id => @technology.id)
     @technology_property_values = @technology.technology_technology_values.collect {|value| value.technology_technology_property}
