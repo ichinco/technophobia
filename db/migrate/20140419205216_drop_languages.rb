7adf1eae0f3c4bbedf8694d3b7be0fdfe2acb7a9
Denise Ichinco <ichinco@gmail.com>
2014-05-02 21:45:12 -0400

    remove another invisible table

--- db/migrate/20140419205216_drop_languages.rb
+++ db/migrate/20140419205216_drop_languages.rb
@@ -4,7 +4,6 @@ class DropLanguages < ActiveRecord::Migration
     drop_table :languages
     drop_table :licenses_technologies
     drop_table :reviews
-    drop_table :technologies_licenses_tables
     drop_table :technology_compatible_with
   end
 end
